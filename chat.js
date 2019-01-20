0e2ec744e17d9233fb756c9419dd87b9c72ed578
dfsek <39248502+dfsek@users.noreply.github.com>
2019-01-20 11:10:15 -0700

    mathjax now successfully styles more

--- chat.js
+++ chat.js
@@ -142,9 +142,10 @@ var integralJson = $.getJSON({
 		if(daysSinceBeginning==17900){
 			Fullintegral="$$\\\int"+Lequation+"dx="+Lintegral+"$$";
 		}
+		document.getElementById("Fullintegral").innerHTML = Fullintegral;
 		MathJax.Hub.Config({messageStyle: "none"});
 		MathJax.Hub.Queue(["Typeset",MathJax.Hub,Lequation]);
-		document.getElementById("Fullintegral").innerHTML = Fullintegral;
+
 		var graph = document.getElementById('graph');
 		draw();
 	}
